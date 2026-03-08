423d5f75e6d66a3598505326af0cc64d769e51bc
mohan13krishna <mohankrishnathalla6@gmail.com>
2026-03-08 14:20:40 +0530

    Update .gitignore for Streamlit configuration

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\k.Rakesh\OneDrive\Desktop\ai\venv"
+set "VIRTUAL_ENV=C:\Users\mohan\OneDrive\Desktop\ai - Copy (2)\ai - Copy\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
